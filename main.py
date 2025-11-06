9390ae8f9d207530b37120cb194d1dcf13870b11
YuanGao <114328932+YuanGao-YG@users.noreply.github.com>
2025-11-06 11:16:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def create_badge_file(label, message, color, filename):
         json.dump(data, f)
 
 # 替换为你的Google Scholar ID
-scholar_id = "4JpRnU4AAAAJ&hl=zh-CN"
+scholar_id = "4JpRnU4AAAAJ"
 stats = get_scholar_stats(scholar_id)
 
 if stats:
